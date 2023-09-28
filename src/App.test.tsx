5bf6596c0fe41dd93bb59d653f6da44f8e628952
solidados <geekbrains.pk@internet.ru>
2023-09-28 22:28:48 +0400

    feat: add simple test for routing and 404 page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,30 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
-import App from './App';
+import { WrappedApp, App } from './App';
 
 describe('App', (): void => {
   it('Renders hello world', (): void => {
-    render(<App />);
+    render(<WrappedApp />);
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
-    ).toHaveTextContent('Hello World');
+    ).toHaveTextContent('Home page');
+  });
+
+  it('Renders NotFound page if invalid path', (): void => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('404');
   });
 });
