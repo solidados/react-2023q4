3119f638cde6570fdd1c35e0b20f13a64c9dd92c
solidados <geekbrains.pk@internet.ru>
2023-09-28 21:54:55 +0400

    feat: install Router and set routing between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App;
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+export default WrappedApp;
