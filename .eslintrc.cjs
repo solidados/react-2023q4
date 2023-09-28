85811a40d7a2a4a171bab57733d385943efaa881
solidados <geekbrains.pk@internet.ru>
2023-09-28 16:31:39 +0400

    feat: install prettier and config it for project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,29 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true
   },
-  "extends": [
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+  'extends': [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
   ],
-  "overrides": [],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  'overrides': [],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+    'project': './tsconfig.json'
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint"
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+    'prettier'
   ],
-  "rules": {
-    "react/react-in-jsx-scope": 0,
+  'rules': {
+    'react/react-in-jsx-scope': 0,
   }
 }
