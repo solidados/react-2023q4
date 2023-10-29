c81355554e347e74919e1dc20230fd4af8ebb5d7
solidados <geekbrains.pk@internet.ru>
2023-10-30 01:22:53 +0400

    feat: add handleErrorButtonClick to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,21 @@ import SearchBar from './SearchBar';
 
 class Header extends Component {
   render() {
+    const handleErrorButtonClick = (): string => {
+      throw new Error('ERROR');
+    };
     return (
       <header className="header">
         <div className="header-container">
           <h1>Movies</h1>
           <SearchBar />
+          <button
+            type="button"
+            className="error-btn"
+            onClick={handleErrorButtonClick}
+          >
+            e!
+          </button>
         </div>
       </header>
     );
