bce62f46903c5e79ccfd50a0acc8d0f9ded0336d
solidados <geekbrains.pk@internet.ru>
2023-11-02 20:21:36 +0400

    feat: migrate class to function for api.ts & CardList.tsx

--- src/helpers/api/api.ts
+++ src/helpers/api/api.ts
@@ -1,16 +1,11 @@
 import { IApiResponse } from '../types/types';
 
-class ApiRequest {
-  private static URL: string = 'https://www.omdbapi.com/?apikey=';
+const URL: string = 'https://www.omdbapi.com/?apikey=';
+const API_KEY: string = '90edd0f9';
 
-  private static API_KEY: string = '90edd0f9';
-
-  static fetchData = async (title: string): Promise<IApiResponse> => {
-    const response = await fetch(
-      `${this.URL}${this.API_KEY}&s=${title}&page=1`
-    );
-    return response.json();
-  };
+async function fetchData(title: string): Promise<IApiResponse> {
+  const response = await fetch(`${URL}${API_KEY}&s=${title}&page=1`);
+  return response.json();
 }
 
-export default ApiRequest;
+export default fetchData;
