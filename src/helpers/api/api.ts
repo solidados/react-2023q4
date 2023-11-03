d8bbc3fd6e08e7aa391c93d6a20cacff32645160
solidados <geekbrains.pk@internet.ru>
2023-11-03 13:38:41 +0400

    refactor: migrate to function api.tsx

--- src/helpers/api/api.ts
+++ src/helpers/api/api.ts
@@ -3,8 +3,13 @@ import { IApiResponse } from '../types/types';
 const URL: string = 'https://www.omdbapi.com/?apikey=';
 const API_KEY: string = '90edd0f9';
 
-async function fetchData(title: string): Promise<IApiResponse> {
-  const response = await fetch(`${URL}${API_KEY}&s=${title}&page=1`);
+async function fetchData(
+  title: string,
+  pageNumber: number = 1
+): Promise<IApiResponse> {
+  const response = await fetch(
+    `${URL}${API_KEY}&s=${title}&page=${pageNumber}`
+  );
   return response.json();
 }
 
