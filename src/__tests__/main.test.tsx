f6afcf083f86de39b80781cc93449bd01463a56a
solidados <geekbrains.pk@internet.ru>
2023-11-13 00:29:16 +0400

    chore: try to test routing

---
+++ src/__tests__/main.test.tsx
@@ -0,0 +1,15 @@
+// import { render, screen } from '@testing-library/react';
+// import { createHistory, createMemorySource } from '@curi/router';
+// import { BrowserRouter } from 'react-router-dom';
+// import Home from '../pages/Home';
+//
+// test('full app rendering/navigating', (): void => {
+//   const source = createMemorySource('/');
+//   const history = createHistory(source);
+//   render(
+//     <BrowserRouter history={history}>
+//       <Home />
+//     </BrowserRouter>
+//   );
+//   expect(screen.getByText(/whatever you want/i)).toBeInTheDocument();
+// });
